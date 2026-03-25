894595eb6c9224d470505aab595788d83ecae544
MyeongSooJang <mmsgod0427@naver.com>
2026-03-26 00:24:51 +0900

    feat: 상품 등록 메소드 생성

---
+++ product/src/main/java/com/example/product/infrastructure/repository/ProductRepositoryImpl.java
@@ -0,0 +1,22 @@
+package com.example.product.infrastructure.repository;
+
+import com.example.product.domain.entity.Product;
+import com.example.product.domain.repository.ProductRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+/**
+ * ProductRepository 구현체
+ * Domain의 Repository Interface를 JPA로 구현
+ */
+@Repository
+@RequiredArgsConstructor
+public class ProductRepositoryImpl implements ProductRepository {
+    private final ProductJpaRepository jpaRepository;
+
+    @Override
+    public Product save(Product product) {
+        return jpaRepository.save(product);
+    }
+
+}
